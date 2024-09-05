d7976650ca70264f10cf4dcdbdbf592661099ff9
Oleh <gend4016@gmail.com>
2024-09-05 22:15:50 +0300

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
   return (
     <html className="light" lang="en">
       <head>
+        
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap"
           rel="stylesheet"
