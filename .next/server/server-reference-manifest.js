f76d59b238866f95d63c0e870c9cff8ed091ab97
Oleh <gend4016@gmail.com>
2024-09-26 10:35:43 +0300

    a

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vDY9K4CdjUkzg2IKv1qERjgGKTYC9LXrOKjOMsTP5cY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"TZrZlK2l8qEznzvCMnDOq+0EvBxDm2w/rLO0rhrz3+M=\"\n}"
