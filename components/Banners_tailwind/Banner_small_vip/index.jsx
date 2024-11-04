0b860c496d028513cfe3c2d161b3735e23dac21a
Oleh <gend4016@gmail.com>
2024-11-04 09:44:46 +0200

    d

--- components/Banners_tailwind/Banner_small_vip/index.jsx
+++ components/Banners_tailwind/Banner_small_vip/index.jsx
@@ -30,7 +30,7 @@ export default function Banner_small() {
         <div className="">
           <div className="flex items-center px-2 py-2.5 sm:px-3.5 rounded-md mt-10 flex-col sm:flex-row new-brands">
             <h2 className="text-3xl font-bold tracking-tight text-white random-title">
-              {t("VIP program")}
+              {t("Best VIP Programs")}
             </h2>
           </div>
         </div>
