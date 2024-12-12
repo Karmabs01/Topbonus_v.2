f22d5444e44525f091256e3dfd26a81014f97ec3
Oleh <gend4016@gmail.com>
2024-12-12 21:53:37 +0200

    vv

--- components/Otp/index.tsx
+++ components/Otp/index.tsx
@@ -29,7 +29,6 @@ const OtpModal: React.FC<OtpModalProps> = ({ onClose }) => {
     setOtpId(null);
     setEmail("");
   };
-
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-96 relative bg-otp">
@@ -51,7 +50,7 @@ const OtpModal: React.FC<OtpModalProps> = ({ onClose }) => {
       </div>
     </div>
   );
-  
+
 };
 
 export default OtpModal;
