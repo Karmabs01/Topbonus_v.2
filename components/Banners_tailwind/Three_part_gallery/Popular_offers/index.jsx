46f154f4579469462d40d8ad9b414d2cbe93cbe9
Oleh <gend4016@gmail.com>
2025-01-21 18:40:41 +0200

    f

--- components/Banners_tailwind/Three_part_gallery/Popular_offers/index.jsx
+++ components/Banners_tailwind/Three_part_gallery/Popular_offers/index.jsx
@@ -21,7 +21,7 @@ export default function Popular_offers() {
   const [loading, setLoading] = useState(true);
 
   // Список брендов, которые вы хотите закрепить
-  const pinnedBrandsList = ["Winbay", "FairPari", "Luckychoo"];
+  const pinnedBrandsList = ["Winbay", "Luckychoo"];
   
   // Массив состояний для закреплённых и остальных
   const [pinnedBrands, setPinnedBrands] = useState([]);
