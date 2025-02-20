5cc510d71476c030be8d7bbcc78edf44bfbc7413
Oleh <gend4016@gmail.com>
2025-02-20 15:11:31 +0200

    f

--- components/Banners_tailwind/Three_part_gallery/Popular_offers/index.jsx
+++ components/Banners_tailwind/Three_part_gallery/Popular_offers/index.jsx
@@ -21,7 +21,7 @@ export default function Popular_offers() {
   const [loading, setLoading] = useState(true);
 
   // Список брендов, которые вы хотите закрепить
-  const pinnedBrandsList = ["Voodoo Casino", "Duospin"];
+  const pinnedBrandsList = ["Voodoo Casino", "Duospin", "Book of Bet", "BeOnBet"];
   
   // Массив состояний для закреплённых и остальных
   const [pinnedBrands, setPinnedBrands] = useState([]);
