56b9a366d3a8cd2b29c29ae8bb53345873e0ba25
Oleh <gend4016@gmail.com>
2025-03-14 17:27:31 +0200

    f

--- components/Banners_tailwind/Advent/index.jsx
+++ components/Banners_tailwind/Advent/index.jsx
@@ -14,7 +14,7 @@ import { getUserData } from "@/components/getUser/getUser";
 const CUSTOM_BRANDS_POOL = [
   "Syndicate casino",
   "Mr.Bet",
-  "Loki",
+  "Loki casino",
   "XON",
   "SpiNight",
   "Winshark",
