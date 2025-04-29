6a9e951b2405b3e87b480de997eefd1303237b76
Oleh <gend4016@gmail.com>
2025-04-29 10:32:50 +0300

    f

--- components/Banners_tailwind/Three_part_gallery/Popular_offers/index.jsx
+++ components/Banners_tailwind/Three_part_gallery/Popular_offers/index.jsx
@@ -21,9 +21,13 @@ export default function Popular_offers() {
   const [loading, setLoading] = useState(true);
 
   // Список брендов, которые вы хотите закрепить
-  const pinnedBrandsList = ["Fast Slots", "Oscarspin", "Nine Casino", "Samba Slots", "Voodoo Casino"];
-
-
+  const pinnedBrandsList = [
+    "Winspirit",
+    "Oscarspin",
+    "Voodoo Casino",
+    "Romibet",
+    "Bonrush",
+  ];
 
   // Массив состояний для закреплённых и остальных
   const [pinnedBrands, setPinnedBrands] = useState([]);
@@ -116,9 +120,13 @@ export default function Popular_offers() {
   const categoryBrands0 = { key1: "Networks", key2: "Premium" };
 
   // Получаем бренды через SWR
-  const { data, error } = useSWR(["brands", language], () => getBrands(language), {
-    initialData: [],
-  });
+  const { data, error } = useSWR(
+    ["brands", language],
+    () => getBrands(language),
+    {
+      initialData: [],
+    }
+  );
 
   let userId = "";
   if (typeof window !== "undefined") {
